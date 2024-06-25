3bf38156ecd9fb45abc24c4d830551b82793c56b
Nuno Salvador <nuno@salvador.com>
2024-06-26 00:04:48 +0100

    Added async implementation of the fetchDummyProductsAll so that it doesnt use the fetchDummyProductsAllPaginatedPublisher anymore

--- IRShowcaseMVP/Common/DataSources/Networking/Fetchables/APIService+FetchDummyProducts.swift
+++ IRShowcaseMVP/Common/DataSources/Networking/Fetchables/APIService+FetchDummyProducts.swift
@@ -30,20 +30,43 @@ extension APIServiceImpl: FetchDummyProductsProtocol {
 
     @Sendable
     func fetchDummyProductsAll() async throws -> (DummyProductDataContainer, DataProviderSource) {
-        var cancellables = Set<AnyCancellable>()
-        return try await withCheckedThrowingContinuation { continuation in
-            fetchDummyProductsAllPaginatedPublisher()
-                .sink { completion in
-                    switch completion {
-                    case .finished:
-                        break
-                    case .failure(let error):
-                        continuation.resume(throwing: error)
-                    }
-                } receiveValue: { value in
-                    continuation.resume(returning: value)
+        try await withThrowingTaskGroup(of: (Int, DummyProductDataContainer?).self) { group in
+            // We need to do at least one fetch to get the total
+            let firstFetch = try await fetchDummyProducts(limit: Constants.DummyProductsAPIPageSize, skip: 0)
+            let (firstPage, _) = firstFetch
+
+            // Build params for the next paginated fetches and then add a task for each fetch into the group.
+            let paginatedParams = APIServiceImpl.buildParamsForThePagesToFetch(
+                pageSize: Constants.DummyProductsAPIPageSize,
+                totalFetched: firstPage.skip + firstPage.limit,
+                total: firstPage.total
+            )
+
+            // We also want to maintain the order of the results so we're going to use one array and save the fetched page by index.
+            var paginatedPages: [DummyProductDataContainer?] = Array(repeating: nil, count: paginatedParams.count + 1)
+
+            // Add the parallel tasks to the group
+            for (index, paginatedParam) in paginatedParams.enumerated() {
+                group.addTask {
+                    return (index, try await fetchDummyProducts(limit: paginatedParam.limit, skip: paginatedParam.skip).0)
                 }
-                .store(in: &cancellables)
+            }
+            
+            // Wait for them and store them in the paginatedPages by index
+            for try await (index, paginatedPage) in group {
+                paginatedPages[index] = paginatedPage
+            }
+
+            let fetchedPages = paginatedPages.compactMap({ $0 })
+            let productsFromFetchedPages = fetchedPages.flatMap({ $0.products })
+
+            let dataContainter = DummyProductDataContainer(
+                total: firstPage.total,
+                skip: firstPage.skip,
+                limit: firstPage.limit,
+                products: firstPage.products + productsFromFetchedPages
+            )
+            return (dataContainter, .remote)
         }
     }
 
@@ -68,6 +91,23 @@ extension APIServiceImpl: FetchDummyProductsProtocol {
 
     func fetchDummyProductsAllPublisher() -> AnyPublisher<(DummyProductDataContainer, DataProviderSource), Error> {
         fetchDummyProductsAllPaginatedPublisher()
+    }
+}
+
+private extension APIServiceImpl {
+    static func buildParamsForThePagesToFetch(pageSize: Int, totalFetched: Int, total: Int) -> [(limit: Int, skip: Int)] {
+        var acum: [(limit: Int, skip: Int)] = []
+
+        var numberOfFetchedElements = totalFetched
+        var hasMorePages = numberOfFetchedElements < total
+
+        while hasMorePages {
+            acum.append((limit: pageSize, skip: numberOfFetchedElements))
+            numberOfFetchedElements += pageSize
+            hasMorePages = numberOfFetchedElements < total
+        }
+
+        return acum
     }
 }
 
